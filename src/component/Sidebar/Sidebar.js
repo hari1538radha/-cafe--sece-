c6ef6e79c942c3ed3979215e209814491f0940ee
Hari1538radha <hari.r2020ece@sece.ac.in>
2023-06-30 19:26:11 +0530

    feat[sidebar]: working on sidebar

--- src/component/Sidebar/Sidebar.js
+++ src/component/Sidebar/Sidebar.js
@@ -1,20 +1,50 @@
 import React from 'react';
 //image import
+import videoicon from "../New folder/Images/vedio.svg"
+import keyboardicon from "../New folder/Images/keyboard.svg"
 import New from '../New folder/Images/New.svg';
-import Inbox from '../New folder/Images/Side Nav icons.svg';
+//const import
+import { sidebardata } from '../const';
 const Sidebar = () => {
   return (
-    <div className="w-[248px] border-r-[0.5px] border-neutral-200 h-screen">
-      <div className="pt-[15px] ml-2">
-        <button className="flex shadow-md font-medium text-[13px] text-[#3D4043]  items-center justify-center w-[126px] h-12 rounded-[40px]">
-          <img className="pr-4" src={New} alt="new"></img>
-          Compose
-        </button>
+    <div className="flex flex-col  justify-between items-start w-[248px] border-r-[0.5px] border-neutral-200 h-screen">
+      <div className="w-full">
+        <div className="pt-[15px] ml-2">
+          <button className="flex shadow-md font-medium text-[13px] text-[#3D4043]  items-center justify-center w-[126px] h-12 rounded-[40px]">
+            <img className="pr-4" src={New} alt="new"></img>
+            Compose
+          </button>
+        </div>
+        <div className="pt-4">
+          {sidebardata.length &&
+            sidebardata.map((obj) => (
+              <div className="flex hover:bg-[#F1F3F4] focus:bg-[#F9E9E7]  pl-6  w-full rounded-br-[32px] h-8 items-center rounded-tr-[32px] ">
+                <img alt="inbox" src={obj.img}></img>
+                <div className="flex w-full items-center justify-between">
+                  <p className="text-[14px] font-medium pl-4">{obj.Title}</p>
+                  {/* {obj.count == 0:(<p></p>? <p className="pr-8"></p>)} */}
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
-      <div className="pt-4">
-        <div className="flex hover:bg-[#F1F3F4] pl-6  rounded-br-[32px] h-8 items-center rounded-tr-[32px] ">
-          <img alt="inbox" src={Inbox}></img>
-          <p className="text-[14px] font-bold pl-4">Inbox</p>
+      <div className="">
+        <div>
+          <p>Meet</p>
+          <div>
+            <img src={videoicon} alt="icon"></img>
+            <p>
+              <div>New meeting</div>
+            </p>
+          </div>
+          <div>
+            <img src={keyboardicon} alt="icon"></img>
+            <p>
+              <div>Join a meeting</div>
+            </p>
+          </div>
+          <p>Hangouts</p>
+          <button>Sign in</button>
         </div>
       </div>
     </div>
---
+++ src/component/const.js
@@ -0,0 +1,58 @@
+//image imports
+import inbox from '../component/New folder/Images/inbox.svg';
+import Star from '../component/New folder/Images/Star.svg';
+import Snooze from '../component/New folder/Images/Snooze.svg';
+import sent from '../component/New folder/Images/sent.svg';
+import draft from '../component/New folder/Images/draft.svg';
+import spam from '../component/New folder/Images/spam.svg';
+import Trash from '../component/New folder/Images/Trash.svg';
+import Label from '../component/New folder/Images/Label.svg';
+import More from '../component/New folder/Images/More.svg';
+
+export const sidebardata = [
+  {
+    img: inbox,
+    Title: 'Inbox',
+    count:1
+  },
+  {
+    img: Star,
+    Title: 'Starred',
+    count:1
+  },
+  {
+    img: Snooze,
+    Title: 'Snoozed',
+    count:1
+  },
+  {
+    img: sent,
+    Title: 'Sent',
+    count:1
+  },
+  {
+    img: draft,
+    Title: 'Drafts',
+    count:1
+  },
+  {
+    img: spam,
+    Title: 'Spam',
+    count:1
+  },
+  {
+    img: Trash,
+    Title: 'Trash',
+    count:1
+  },
+  {
+    img: Label,
+    Title: 'Categories',
+    count:1
+  },
+  {
+    img: More,
+    Title: 'More',
+    count:1
+  },
+];
